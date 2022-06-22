70827f6c084099624a04220f3bb0a25871fd3758
Gavin Casey <gavinjc73@gmail.com>
2022-06-22 14:41:27 -0500

    the beginning of the end

---
+++ src/main/java/com/dpi/publishingapi/PublishingApiApplication.java
@@ -0,0 +1,13 @@
+package com.dpi.publishingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublishingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PublishingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dpi/publishingapi/PublishingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dpi.publishingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublishingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
