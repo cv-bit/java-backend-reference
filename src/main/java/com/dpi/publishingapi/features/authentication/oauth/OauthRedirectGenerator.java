514221c296294d3737c6ebcd1a416bae84bf74ee
Gavin Casey <gavinjc73@gmail.com>
2022-07-20 08:53:22 -0500

    fixed oauth refresh bug

--- src/main/java/com/dpi/publishingapi/features/authentication/oauth/OauthRedirectGenerator.java
+++ src/main/java/com/dpi/publishingapi/features/authentication/oauth/OauthRedirectGenerator.java
@@ -1,5 +1,7 @@
 package com.dpi.publishingapi.features.authentication.oauth;
 
+import com.dpi.publishingapi.data.auth.refresh.RefreshToken;
+import com.dpi.publishingapi.data.auth.refresh.RefreshTokenRepository;
 import com.dpi.publishingapi.data.auth.user.User;
 import com.dpi.publishingapi.features.authentication.jwt.JwtTokenGenerator;
 import com.dpi.publishingapi.features.authentication.refresh.RefreshTokenGenerator;
@@ -12,20 +14,23 @@ public class OauthRedirectGenerator {
 
     private final JwtTokenGenerator jwtTokenGenerator;
     private final RefreshTokenGenerator refreshTokenGenerator;
+    private final RefreshTokenRepository refreshTokenRepository;
 
     @Autowired
-    public OauthRedirectGenerator(JwtTokenGenerator jwtTokenGenerator, RefreshTokenGenerator refreshTokenGenerator) {
+    public OauthRedirectGenerator(JwtTokenGenerator jwtTokenGenerator, RefreshTokenGenerator refreshTokenGenerator, RefreshTokenRepository refreshTokenRepository) {
         this.jwtTokenGenerator = jwtTokenGenerator;
         this.refreshTokenGenerator = refreshTokenGenerator;
+        this.refreshTokenRepository = refreshTokenRepository;
     }
 
     public String generateUrl(User user) {
         String token = jwtTokenGenerator.generate(user.getEmail());
-        String refreshToken = refreshTokenGenerator.generate(user).getToken();
+        RefreshToken refreshToken = refreshTokenGenerator.generate(user);
+        refreshTokenRepository.save(refreshToken);
 
         String url = UriComponentsBuilder.fromUriString("http://localhost:3000/login/oauth/redirect")
                 .queryParam("token", token)
-                .queryParam("refresh", refreshToken)
+                .queryParam("refresh", refreshToken.getToken())
                 .queryParam("email", user.getEmail())
                 .build().toUriString();
 
